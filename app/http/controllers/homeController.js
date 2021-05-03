ec343cd5226a5ef0b915be531e42bd8fec7fd1ac
Thanveer Nawaz <thanveernawaz094@gmail.com>
2021-05-03 16:05:41 +0530

    Db Fetch

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,9 +1,15 @@
+const Menu = require('../../models/menu')
+
+
 function homeController(){
     return {
-        index(req,res){
-            res.render('home')
+        async index(req,res){
+            const pizzas = await Menu.find()
+            
+            return res.render('home',{pizzas: pizzas})
+            
         }
-    }
+    }   
 }
 
 
